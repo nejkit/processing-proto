c0323a3910b2431f97798a3c3aa0ac7eddbcaf59
nikita.bacherikov <bacherikovn@gmail.com>
2023-11-09 00:19:17 +0200

    add protos

--- balances/Common.pb.go
+++ balances/Common.pb.go
@@ -162,6 +162,116 @@ func (x *WalletInfo) GetBalanceInfos() []*BalanceInfo {
 	return nil
 }
 
+type GetWalletInfoRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id      string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Address string `protobuf:"bytes,2,opt,name=address,proto3" json:"address,omitempty"`
+}
+
+func (x *GetWalletInfoRequest) Reset() {
+	*x = GetWalletInfoRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_balances_Common_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetWalletInfoRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetWalletInfoRequest) ProtoMessage() {}
+
+func (x *GetWalletInfoRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_balances_Common_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetWalletInfoRequest.ProtoReflect.Descriptor instead.
+func (*GetWalletInfoRequest) Descriptor() ([]byte, []int) {
+	return file_balances_Common_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *GetWalletInfoRequest) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *GetWalletInfoRequest) GetAddress() string {
+	if x != nil {
+		return x.Address
+	}
+	return ""
+}
+
+type GetWalletInfoResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id         string      `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	WalletInfo *WalletInfo `protobuf:"bytes,2,opt,name=wallet_info,json=walletInfo,proto3" json:"wallet_info,omitempty"`
+}
+
+func (x *GetWalletInfoResponse) Reset() {
+	*x = GetWalletInfoResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_balances_Common_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetWalletInfoResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetWalletInfoResponse) ProtoMessage() {}
+
+func (x *GetWalletInfoResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_balances_Common_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetWalletInfoResponse.ProtoReflect.Descriptor instead.
+func (*GetWalletInfoResponse) Descriptor() ([]byte, []int) {
+	return file_balances_Common_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *GetWalletInfoResponse) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *GetWalletInfoResponse) GetWalletInfo() *WalletInfo {
+	if x != nil {
+		return x.WalletInfo
+	}
+	return nil
+}
+
 var File_balances_Common_proto protoreflect.FileDescriptor
 
 var file_balances_Common_proto_rawDesc = []byte{
@@ -184,8 +294,18 @@ var file_balances_Common_proto_rawDesc = []byte{
 	0x74, 0x65, 0x64, 0x12, 0x31, 0x0a, 0x0d, 0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x5f, 0x69,
 	0x6e, 0x66, 0x6f, 0x73, 0x18, 0x04, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0c, 0x2e, 0x42, 0x61, 0x6c,
 	0x61, 0x6e, 0x63, 0x65, 0x49, 0x6e, 0x66, 0x6f, 0x52, 0x0c, 0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63,
-	0x65, 0x49, 0x6e, 0x66, 0x6f, 0x73, 0x42, 0x0b, 0x5a, 0x09, 0x2f, 0x62, 0x61, 0x6c, 0x61, 0x6e,
-	0x63, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x65, 0x49, 0x6e, 0x66, 0x6f, 0x73, 0x22, 0x40, 0x0a, 0x14, 0x47, 0x65, 0x74, 0x57, 0x61, 0x6c,
+	0x6c, 0x65, 0x74, 0x49, 0x6e, 0x66, 0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x0e,
+	0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x18,
+	0x0a, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x22, 0x55, 0x0a, 0x15, 0x47, 0x65, 0x74, 0x57,
+	0x61, 0x6c, 0x6c, 0x65, 0x74, 0x49, 0x6e, 0x66, 0x6f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
+	0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69,
+	0x64, 0x12, 0x2c, 0x0a, 0x0b, 0x77, 0x61, 0x6c, 0x6c, 0x65, 0x74, 0x5f, 0x69, 0x6e, 0x66, 0x6f,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x0b, 0x2e, 0x57, 0x61, 0x6c, 0x6c, 0x65, 0x74, 0x49,
+	0x6e, 0x66, 0x6f, 0x52, 0x0a, 0x77, 0x61, 0x6c, 0x6c, 0x65, 0x74, 0x49, 0x6e, 0x66, 0x6f, 0x42,
+	0x0b, 0x5a, 0x09, 0x2f, 0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -200,18 +320,21 @@ func file_balances_Common_proto_rawDescGZIP() []byte {
 	return file_balances_Common_proto_rawDescData
 }
 
-var file_balances_Common_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_balances_Common_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_balances_Common_proto_goTypes = []interface{}{
-	(*BalanceInfo)(nil), // 0: BalanceInfo
-	(*WalletInfo)(nil),  // 1: WalletInfo
+	(*BalanceInfo)(nil),           // 0: BalanceInfo
+	(*WalletInfo)(nil),            // 1: WalletInfo
+	(*GetWalletInfoRequest)(nil),  // 2: GetWalletInfoRequest
+	(*GetWalletInfoResponse)(nil), // 3: GetWalletInfoResponse
 }
 var file_balances_Common_proto_depIdxs = []int32{
 	0, // 0: WalletInfo.balance_infos:type_name -> BalanceInfo
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	1, // 1: GetWalletInfoResponse.wallet_info:type_name -> WalletInfo
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_balances_Common_proto_init() }
@@ -244,6 +367,30 @@ func file_balances_Common_proto_init() {
 				return nil
 			}
 		}
+		file_balances_Common_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GetWalletInfoRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_balances_Common_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GetWalletInfoResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -251,7 +398,7 @@ func file_balances_Common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_balances_Common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
